0acee203df8033db180201d4e1f9dfa9f0eeb061
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-12 03:57:57 -0600

    feat: create dataBaseStatic

---
+++ lib/service/data_static.dart
@@ -0,0 +1,9 @@
+import 'package:yonesto_ui/models/product.dart';
+
+class DataBaseStatic {
+  List<Product> products = [];
+  //UserAdmin activeUserAdmin = UserAdmin(userName: '', password: '', id: '');
+  //List<ComputerRoomUI> computerRooms = [];
+}
+
+DataBaseStatic databaseStatic = DataBaseStatic();
