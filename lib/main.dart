19a9841902dd6d218cc2bdea49271918b400fabb
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-12 15:11:14 -0600

    feat: load .envs

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:yonesto_ui/ui/app.dart';
 
-void main() {
+void main() async {
   setPathUrlStrategy();
+  await dotenv.load(fileName: ".env");
   runApp(const YonestoApp());
 }
