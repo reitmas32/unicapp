1d90dae09e2e78a96f893dc7bdf198b2e670c049
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-12 15:12:57 -0600

    feat: add jwt and userName in DataStatic

--- lib/service/data_static.dart
+++ lib/service/data_static.dart
@@ -4,6 +4,8 @@ class DataBaseStatic {
   List<Product> products = [];
   //UserAdmin activeUserAdmin = UserAdmin(userName: '', password: '', id: '');
   //List<ComputerRoomUI> computerRooms = [];
+  String jwt = '';
+  String userName = '';
 }
 
 DataBaseStatic databaseStatic = DataBaseStatic();
