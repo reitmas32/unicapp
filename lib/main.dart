1cc5d1207e342fd3b50d4df8b54eacb85851de2d
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-14 06:44:19 -0600

    feat: remove unsude call

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:url_strategy/url_strategy.dart';
-import 'package:yonesto_ui/service/apis/api_conection.dart';
 import 'package:yonesto_ui/ui/app.dart';
 
 void main() async {
   setPathUrlStrategy();
   await dotenv.load(fileName: ".env");
-  await yonestoAPI.getProducts();
   runApp(const YonestoApp());
 }
