1628121a189eea871fad32932766ed96f18541e2
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-15 17:46:11 -0600

    refactor: rename file

--- lib/service/apis/api_conection.dart
+++ lib/service/apis/api_conection.dart
@@ -1,5 +1,5 @@
 import 'package:yonesto_ui/service/apis/uniaccounts/api_uniaccounts.dart';
-import 'package:yonesto_ui/service/apis/yonesto/api_connection.dart';
+import 'package:yonesto_ui/service/apis/yonesto/api_yonesto.dart';
 
 AccountAPI uniaccountsAPI = AccountAPI();
 YonestoAPI yonestoAPI = YonestoAPI();
