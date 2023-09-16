aa1f2a3ac8cbd8b6c8f7e9b854a2f9681694feef
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-15 22:25:16 -0600

    feat: create funcs by conect with secure_storage

--- lib/service/data_static.dart
+++ lib/service/data_static.dart
@@ -1,14 +1,51 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:yonesto_ui/models/product.dart';
 
-class DataBaseStatic {
+class StorageConection {
   List<Product> products = [];
   //UserAdmin activeUserAdmin = UserAdmin(userName: '', password: '', id: '');
   //List<ComputerRoomUI> computerRooms = [];
   String jwt = '';
   String userName = '';
 
-  static final storage = FlutterSecureStorage();
+  static const storage = FlutterSecureStorage();
+  static Future<bool> isSessionActive() async {
+    String jwt = await StorageConection.storage.read(key: 'jwt') ?? '';
+
+    bool response = jwt == '';
+    return Future(() => response);
+  }
+
+  static Future<bool> writeJWT(String jwt) async {
+    try {
+      await StorageConection.storage.write(
+        key: 'jwt',
+        value: jwt,
+      );
+      return Future(() => true);
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return Future(() => false);
+    }
+  }
+
+  static Future<bool> cleanJWT() async {
+    try {
+      await StorageConection.storage.write(
+        key: 'jwt',
+        value: '',
+      );
+      return Future(() => true);
+    } catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+      return Future(() => false);
+    }
+  }
 }
 
-DataBaseStatic databaseStatic = DataBaseStatic();
+StorageConection databaseStatic = StorageConection();
