baf0e08ccd8346f97b4bc3436a25375caf72f9d8
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-16 01:16:56 -0600

    feat: remove SnackBar notification

--- lib/ui/widgets/add_to_cart_button.dart
+++ lib/ui/widgets/add_to_cart_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:unihacks_ui_kit/themes/theme_provider.dart';
 import 'package:yonesto_ui/models/product.dart';
 import 'package:yonesto_ui/providers/cart.dart';
 
@@ -17,29 +16,11 @@ class AddToCartButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final cart = Provider.of<CartProvider>(context);
     final size = MediaQuery.of(context).size;
-    final currentColor = Provider.of<ThemeProvider>(context);
 
     return InkWell(
       onTap: avalible
           ? () {
               cart.addToCart(product);
-              var snackBar = SnackBar(
-                content: Text(
-                  '¡Se Añadio un ${product.name} al Carrito!',
-                  style: TextStyle(
-                    color: currentColor.isDarkTheme()
-                        ? const Color.fromARGB(255, 199, 197, 197)
-                        : const Color.fromARGB(255, 35, 34, 34),
-                  ),
-                ),
-                duration: const Duration(milliseconds: 750),
-                backgroundColor: currentColor.isDarkTheme()
-                    ? const Color.fromARGB(255, 35, 34, 34)
-                    : const Color.fromARGB(
-                        255, 199, 197, 197), // Duración de la visibilidad
-              );
-
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
             }
           : null,
       child: Padding(
