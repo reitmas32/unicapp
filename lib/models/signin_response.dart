4383ccde9969b0c7a3d68b6aed617ce599bd4606
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-15 22:03:08 -0600

    feat: create SignInResponse Model

---
+++ lib/models/signin_response.dart
@@ -0,0 +1,42 @@
+class SignInResponse {
+  bool success;
+  String? message;
+  String? jwt;
+  // List<Product> data;
+
+  SignInResponse({
+    required this.success,
+    this.message,
+    this.jwt,
+    //required this.data,
+  });
+
+  factory SignInResponse.fromJson(Map<String, dynamic> map) {
+    final success = map['Success'] as bool;
+    final message = map['Message'] as String;
+    final jwt = map['Data']?['jwt'] as String;
+
+    return SignInResponse(
+      success: success,
+      message: message,
+      jwt: jwt,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'Success': success,
+      'Message': message,
+      'Data': {
+        'jwt': jwt,
+      }
+      //'Data': data.map((product) => product.toJson()).toList(),
+    };
+  }
+
+  @override
+  String toString() {
+    return '';
+    //return 'SignInResponse(success: $success, message: $message, data: $data,)';
+  }
+}
