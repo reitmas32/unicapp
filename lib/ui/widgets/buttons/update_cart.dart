58d7506caa93b8c6844265b1b5bbdf2b46162ad6
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-17 05:45:57 -0600

    feat: rename and move widget

---
+++ lib/ui/widgets/buttons/update_cart.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class UpdateCartButton extends StatelessWidget {
+  const UpdateCartButton({
+    super.key,
+    required this.iconData,
+    required this.avalible,
+    this.onTap,
+  });
+
+  final bool avalible;
+  final VoidCallback? onTap;
+  final IconData iconData;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return InkWell(
+      onTap: avalible ? onTap : null,
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Container(
+          width: size.width > 750 ? 40 : 25,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5.0),
+            color: avalible
+                ? Colors.purple
+                : const Color.fromARGB(255, 101, 69, 107),
+          ),
+          child: Icon(
+            iconData,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
