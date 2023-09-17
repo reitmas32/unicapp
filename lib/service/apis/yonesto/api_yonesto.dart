c23fff128d4575add79f979ad3a62eb8adcbd785
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-17 07:39:57 -0600

    feat: cretae getUnpaidBuys and payDebts

--- lib/service/apis/yonesto/api_yonesto.dart
+++ lib/service/apis/yonesto/api_yonesto.dart
@@ -3,9 +3,11 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:yonesto_ui/models/buy_request.dart';
+import 'package:yonesto_ui/models/pay_debts_response.dart';
 import 'package:yonesto_ui/models/product_response.dart';
+import 'package:yonesto_ui/models/unpaid_buy_response.dart';
 import 'package:yonesto_ui/service/apis/yonesto/base.dart';
-import 'package:yonesto_ui/service/data_static.dart'; // Asegúrate de importar tus modelos aquí
+import 'package:yonesto_ui/service/data_static.dart';
 
 class YonestoAPI {
   final Map<String, String> headers = {
@@ -59,5 +61,61 @@ class YonestoAPI {
     }
 
     return true;
+  }
+
+  Future<UnpaidBuyResponse> getUnpaidBuys(int code) async {
+    var responseProcess = UnpaidBuyResponse(
+      numberBuys: 0,
+      successRequest: false,
+      totalRemainingAmount: 0.0,
+    );
+    try {
+      final response = await http.get(
+        Uri.parse('${YonestoBase.url}/users/unpaid-buys/$code'),
+        headers: headers,
+      );
+      Map<String, dynamic> responseData = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        responseProcess.successRequest = true;
+        responseProcess.numberBuys =
+            responseData['Data']?['number_buys'].toInt();
+        responseProcess.totalRemainingAmount =
+            responseData['Data']?['total_remaining_amount'];
+      }
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+
+    return responseProcess;
+  }
+
+  Future<PayDebtsResponse> payDebts(int code, double pay) async {
+    var responseProcess = PayDebtsResponse(
+      userCode: code,
+      pay: pay,
+      successRequest: false,
+    );
+    try {
+      final response = await http.post(
+        Uri.parse('${YonestoBase.url}/users/pay-buys'),
+        body: jsonEncode({
+          'code': code,
+          'payment': pay,
+        }),
+        headers: headers,
+      );
+      Map<String, dynamic> responseData = jsonDecode(response.body);
+      if (response.statusCode == 200) {
+        responseProcess.successRequest = true;
+      }
+    } on Exception catch (e) {
+      if (kDebugMode) {
+        print(e);
+      }
+    }
+
+    return responseProcess;
   }
 }
