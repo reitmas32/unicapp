748260404eba041e8d99c71e732edb2ce8fdaecf
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-17 23:17:14 -0600

    feat: create hash of code

--- lib/service/data_static.dart
+++ lib/service/data_static.dart
@@ -84,7 +84,6 @@ class StorageConection {
   }
 
   static String decodeCode(String password) {
-    /*
     String code_0 = StorageConection.hash[password[0]] ?? '';
     String code_1 = StorageConection.hash[password[2]] ?? '';
     String code_2 = StorageConection.hash[password[4]] ?? '';
@@ -92,21 +91,21 @@ class StorageConection {
     String code_4 = StorageConection.hash[password[8]] ?? '';
 
     String code = code_0 + code_1 + code_2 + code_3 + code_4;
-    */
-    return '10064';
+
+    return code;
   }
 
   static const Map<String, String> hash = {
-    '0': 'a',
-    '1': 'b',
-    '2': 'c',
-    '3': 'd',
-    '4': 'e',
-    '5': 'f',
-    '6': 'g',
-    '7': 'h',
-    '8': 'i',
-    '9': 'j',
+    'a': '0',
+    'b': '1',
+    'c': '2',
+    'd': '3',
+    'e': '4',
+    'f': '5',
+    'g': '6',
+    'h': '7',
+    'i': '8',
+    'j': '9',
   };
 }
 
