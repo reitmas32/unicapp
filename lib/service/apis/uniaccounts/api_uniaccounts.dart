7275a558fccf2fa277e2bfa9250509f69176efcc
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-19 23:20:21 -0600

    refactor: change type of return to ProccessResponce

--- lib/service/apis/uniaccounts/api_uniaccounts.dart
+++ lib/service/apis/uniaccounts/api_uniaccounts.dart
@@ -1,57 +1,68 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
+import 'package:yonesto_ui/domain/models/proccess_response/proccess_response.dart';
+import 'package:yonesto_ui/domain/models/user/user_getway.dart';
 import 'package:yonesto_ui/models/signin_response.dart';
-import 'package:yonesto_ui/models/user.dart';
+import 'package:yonesto_ui/domain/models/user/user.dart';
 import 'package:yonesto_ui/service/apis/uniaccounts/base.dart';
-import 'package:yonesto_ui/service/data_static.dart';
 
-class AccountAPI {
+class AccountAPI extends AccontsUserAPI {
   final Map<String, String> headers = {
     'Content-Type': 'application/json',
     'Api-Key': dotenv.env['UNIACCONTS_API_KEY'] ?? '',
     'Service': dotenv.env['SERVICE_NAME'] ?? '',
   };
 
+  AccountAPI({required super.storage});
+
   Future<bool> signUp(User user) async {
     return Future(() => false);
   }
 
-  Future<int> signIn(User user) async {
+  @override
+  Future<ProccessResponce> singIn(User user) async {
+    var responseProccess = ProccessResponce(
+      success: false,
+      data: '',
+      code: 500,
+    );
     try {
+      // Create Body by Request
       final String bodyRequestSignIn = jsonEncode({
         "user_name": user.userName,
         "password": user.password,
       });
+
+      // Make the Request
       final http.Response response = await http.put(
         Uri.parse('${UniaccountsBase.url}/signin'),
         headers: headers,
         body: bodyRequestSignIn,
       );
 
+      // Proccess if THe Request is Successfull
       if (response.statusCode == 200) {
+        // Decode Response
         final Map<String, dynamic> responseData = jsonDecode(response.body);
 
+        // Get Data
         final SignInResponse signInResponse =
             SignInResponse.fromJson(responseData);
+        if (signInResponse.success) {
+          user.jwt = signInResponse.jwt!;
+          await storage.saveJWT(signInResponse.jwt!);
+          await storage.saveUserName(user.userName);
+          responseProccess.data = signInResponse;
 
-        final bool success = signInResponse.success;
-        if (success) {
-          databaseStatic.jwt = responseData['Data']['token_jwt'];
-          databaseStatic.userName = user.userName;
-          StorageConection.writeJWT(databaseStatic.jwt);
-          String code = StorageConection.decodeCode(user.password);
-          StorageConection.writeCode(code.toString());
+          responseProccess.success = signInResponse.success;
         }
+        responseProccess.code = response.statusCode;
       }
-
-      return response.statusCode;
-    } catch (e) {
-      if (kDebugMode) {
-        print('Error en la solicitud: $e');
-      }
-      return -1; // Devuelve un valor negativo para indicar un error
+    } on Exception catch (e) {
+      responseProccess.data = e.toString();
     }
+
+    return Future(() => responseProccess);
   }
 }
