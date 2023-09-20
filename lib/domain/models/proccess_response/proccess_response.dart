d5bf8a7fb3f321ce9c621893f790f6b758999d09
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-19 22:50:00 -0600

    feat: Create a Standar by Responce Service -> UI

---
+++ lib/domain/models/proccess_response/proccess_response.dart
@@ -0,0 +1,11 @@
+class ProccessResponce {
+  bool success;
+  dynamic data;
+  int code;
+
+  ProccessResponce({
+    required this.success,
+    required this.data,
+    required this.code,
+  });
+}
