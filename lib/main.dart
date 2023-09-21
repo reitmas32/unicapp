2f045901a13ce2843fbe750d2af377ef5ca0314a
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-21 07:19:16 -0600

    feat: add ProviderScope by Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_strategy/url_strategy.dart';
 import 'package:yonesto_ui/ui/app.dart';
 
 void main() async {
   setPathUrlStrategy();
   await dotenv.load(fileName: ".env");
-  runApp(const YonestoApp());
+  runApp(const ProviderScope(child: YonestoApp()));
 }
