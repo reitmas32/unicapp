8d1231f7505d72a0da35a3e91ca1b04c454d5b28
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-21 07:20:37 -0600

    feat: convert to ConsumerStatefulWidget

--- lib/ui/yonesto.dart
+++ lib/ui/yonesto.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:unihacks_ui_kit/themes/theme_provider.dart';
 import 'package:unihacks_ui_kit/themes/themes.dart';
+import 'package:yonesto_ui/providers/providers.dart';
 import 'package:yonesto_ui/ui/pages/cart.dart';
 import 'package:yonesto_ui/ui/pages/error.dart';
 import 'package:yonesto_ui/ui/pages/home.dart';
@@ -21,16 +23,16 @@ CustomTransitionPage buildPageWithDefaultTransition<T>({
   );
 }
 
-class Yonesto extends StatefulWidget {
+class Yonesto extends ConsumerStatefulWidget {
   const Yonesto({
     super.key,
   });
 
   @override
-  State<Yonesto> createState() => _YonestoState();
+  ConsumerState<Yonesto> createState() => _YonestoState();
 }
 
-class _YonestoState extends State<Yonesto> {
+class _YonestoState extends ConsumerState<Yonesto> {
   final GoRouter _router = GoRouter(
     routes: [
       GoRoute(
@@ -61,14 +63,12 @@ class _YonestoState extends State<Yonesto> {
 
   @override
   Widget build(BuildContext context) {
-    final currentTheme = Provider.of<ThemeProvider>(context);
+    final isDarkMode = ref.watch(isDarkModeProvider);
 
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       routerConfig: _router,
-      theme: currentTheme.isDarkTheme()
-          ? AppTheme.uniDarkTheme
-          : AppTheme.uniLightTheme,
+      theme: isDarkMode ? AppTheme.uniDarkTheme : AppTheme.uniLightTheme,
       darkTheme: AppTheme.uniDarkTheme,
       themeMode: ThemeMode.light,
     );
