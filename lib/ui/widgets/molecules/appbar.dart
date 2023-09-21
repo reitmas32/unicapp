5f7e9f5a786edf2615a96a760f09cdece2d84678
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-21 07:22:35 -0600

    feat: extract appbar

---
+++ lib/ui/widgets/molecules/appbar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yonesto_ui/providers/providers.dart';
+
+class YonestoSearchBar extends ConsumerWidget {
+  const YonestoSearchBar({super.key, this.onChanged});
+
+  final void Function(String)? onChanged;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(isDarkModeProvider);
+    return Padding(
+      padding: const EdgeInsets.all(25.0),
+      child: TextField(
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: isDarkMode
+              ? const Color.fromARGB(255, 35, 34, 34)
+              : const Color.fromARGB(255, 199, 197, 197),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide.none,
+          ),
+          hintText: "eg: CocaCola",
+          suffixIcon: const Icon(
+            Icons.search,
+            color: Colors.purple,
+          ),
+        ),
+      ),
+    );
+  }
+}
