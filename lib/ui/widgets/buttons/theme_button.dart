e941c9dcb8c6b899ed257375d72908fe87133688
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-21 07:22:45 -0600

    feat: ConsumerWidget and use riverpod

--- lib/ui/widgets/buttons/theme_button.dart
+++ lib/ui/widgets/buttons/theme_button.dart
@@ -1,35 +1,21 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:unihacks_ui_kit/themes/theme_preference.dart';
-import 'package:unihacks_ui_kit/themes/theme_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yonesto_ui/providers/providers.dart';
 
-class ThemeButton extends StatefulWidget {
+class ThemeButton extends ConsumerWidget {
   const ThemeButton({super.key});
 
   @override
-  State<ThemeButton> createState() => _ThemeButtonState();
-}
-
-class _ThemeButtonState extends State<ThemeButton> {
-  @override
-  Widget build(BuildContext context) {
-    final currentTheme = Provider.of<ThemeProvider>(context);
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isDarkMode = ref.watch(isDarkModeProvider);
 
     return IconButton(
       onPressed: () {
-        if (currentTheme.isDarkTheme()) {
-          setState(() {
-            currentTheme.setTheme = ThemePreference.LIGHT;
-          });
-        } else {
-          setState(() {
-            currentTheme.setTheme = ThemePreference.DARK;
-          });
-        }
+        ref.read(isDarkModeProvider.notifier).update((state) => !state);
       },
       icon: Icon(
-        currentTheme.isDarkTheme() ? Icons.wb_sunny : EvaIcons.moon,
+        isDarkMode ? Icons.wb_sunny : EvaIcons.moon,
       ),
     );
   }
