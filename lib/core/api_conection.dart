0cd52967bce02875ddca383a593048eb0537eaf2
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-24 00:05:51 -0600

    refactor: move conection to core/uniaccounts/services

--- lib/core/api_conection.dart
+++ lib/core/api_conection.dart
@@ -1,4 +1,4 @@
-import 'package:unicapp/core/uniaccounts/services/uniaccounts/api_uniaccounts.dart';
+import 'package:unicapp/core/uniaccounts/services/api_uniaccounts.dart';
 import 'package:unicapp/service/apis/yonesto/api_yonesto.dart';
 import 'package:unicapp/service/storages/product_storage.dart';
 import 'package:unicapp/service/storages/user_storage.dart';
