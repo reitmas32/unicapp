26e596906ee5b7256262d116885ed5772ea94cc2
reitmas32 <rafa.zamora.rals@gmail.com>
2023-09-27 05:36:45 -0600

    feat: make responsive the component

--- lib/ui/widgets/input/minimalist_text_filed.dart
+++ lib/ui/widgets/input/minimalist_text_filed.dart
@@ -30,64 +30,68 @@ class _MinimalistTextFieldState extends State<MinimalistTextField> {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.symmetric(
         horizontal: 50,
         vertical: 15,
       ),
-      child: TextFormField(
-        controller: widget.controller,
-        obscureText: _obscureText,
-        onChanged: widget.onChanged,
-        keyboardType: widget.onlyNumbers ? TextInputType.number : null,
-        decoration: InputDecoration(
-          labelText: widget.lable,
-          contentPadding:
-              const EdgeInsets.symmetric(vertical: 10, horizontal: 6),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: const BorderSide(color: Colors.red),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: const BorderSide(color: Colors.purple, width: 2),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: BorderSide(
+      child: SizedBox(
+        width: size.width > 750 ? size.width / 3 : null ,
+        child: TextFormField(
+          controller: widget.controller,
+          obscureText: _obscureText,
+          onChanged: widget.onChanged,
+          keyboardType: widget.onlyNumbers ? TextInputType.number : null,
+          decoration: InputDecoration(
+            labelText: widget.lable,
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 10, horizontal: 6),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: const BorderSide(color: Colors.red),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: const BorderSide(color: Colors.purple, width: 2),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: BorderSide(
+                color: Theme.of(context).colorScheme.onPrimary,
+              ),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25.0),
+              borderSide: const BorderSide(color: Colors.purple),
+            ),
+            labelStyle: TextStyle(
+              fontSize: 15.0,
               color: Theme.of(context).colorScheme.onPrimary,
             ),
-          ),
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(25.0),
-            borderSide: const BorderSide(color: Colors.purple),
-          ),
-          labelStyle: TextStyle(
-            fontSize: 15.0,
-            color: Theme.of(context).colorScheme.onPrimary,
-          ),
-          suffixIcon: widget.isPassword
-              ? IconButton(
-                  icon: Icon(
-                    _obscureText ? Icons.visibility : Icons.visibility_off,
-                    color: _obscureText
-                        ? Colors.purple
-                        : const Color.fromARGB(197, 126, 67, 136),
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _obscureText = !_obscureText;
-                    });
-                  },
-                )
-              : null,
-          filled: true,
-          fillColor: Colors.transparent,
-          counterStyle: const TextStyle(
-            color: Colors.red,
-          ),
-          hintStyle: const TextStyle(
-            color: Colors.red,
+            suffixIcon: widget.isPassword
+                ? IconButton(
+                    icon: Icon(
+                      _obscureText ? Icons.visibility : Icons.visibility_off,
+                      color: _obscureText
+                          ? Colors.purple
+                          : const Color.fromARGB(197, 126, 67, 136),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _obscureText = !_obscureText;
+                      });
+                    },
+                  )
+                : null,
+            filled: true,
+            fillColor: Colors.transparent,
+            counterStyle: const TextStyle(
+              color: Colors.red,
+            ),
+            hintStyle: const TextStyle(
+              color: Colors.red,
+            ),
           ),
         ),
       ),
